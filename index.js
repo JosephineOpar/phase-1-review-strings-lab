576c124d78d6a64decf516594b973598ae363cfa
Josephine Opar <josephineopar@gmail.com>
2023-01-13 20:57:49 +0200

    commit tree 48b2c11d8f5c4b015e7cdecaca57b5d1948f8f98

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Moringa";
+// function welcomeMessage(currentUser){
+//     return `Welcome to Flatbook, ${currentUser}!`;
+// }
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, M!`; 
